978ae6dbe6ec7c2530207bc6d9c86c81c029367d
Derwing Medina <derwing.medina@gmail.com>
2022-12-14 12:14:37 -0500

    new course

--- src/app/pages/latest-courses/latest-courses.component.ts
+++ src/app/pages/latest-courses/latest-courses.component.ts
@@ -40,6 +40,12 @@ export class LatestCoursesComponent implements DoCheck {
       imgWidth: 150,
       imgHeight: 150
     },
+    {
+      name: 'Programming Logic',
+      img: 'programming.png',
+      imgWidth: 150,
+      imgHeight: 150
+    },
   ]
 
   display: boolean = false;
